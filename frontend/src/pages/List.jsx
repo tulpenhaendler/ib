442d08e04733f3055af0dcfb4da9d060b119c3af
Johann <Johann Tanzer>
2026-01-15 19:31:12 +0100

    Add CLI download links to home page

--- frontend/src/pages/List.jsx
+++ frontend/src/pages/List.jsx
@@ -121,6 +121,37 @@ export function List() {
           )
         })
       )}
+
+      <div class="cli-section">
+        <h3>CLI Client</h3>
+        <p>Download the CLI to create and restore backups:</p>
+        <div class="cli-downloads">
+          <a href="/cli/linux/amd64" class="cli-link">
+            <DownloadIcon /> Linux (x64)
+          </a>
+          <a href="/cli/linux/arm64" class="cli-link">
+            <DownloadIcon /> Linux (ARM64)
+          </a>
+          <a href="/cli/darwin/arm64" class="cli-link">
+            <DownloadIcon /> macOS (Apple Silicon)
+          </a>
+          <a href="/cli/darwin/amd64" class="cli-link">
+            <DownloadIcon /> macOS (Intel)
+          </a>
+          <a href="/cli/windows/amd64" class="cli-link">
+            <DownloadIcon /> Windows (x64)
+          </a>
+        </div>
+      </div>
     </div>
+  )
+}
+
+function DownloadIcon() {
+  return (
+    <svg width="14" height="14" fill="currentColor" viewBox="0 0 16 16">
+      <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
+      <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
+    </svg>
   )
 }
